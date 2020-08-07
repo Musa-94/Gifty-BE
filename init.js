830b74a267e2286e9f1481d9bba12fa472a4ff1a
MusaW <musa@wizardsdev.com>
2020-08-07 19:47:25 +0300

    add procfile

--- init.js
+++ init.js
@@ -1,7 +1,7 @@
 const Server = require('./src/Server');
 
 function init() {
-    this.port = 2020;
+    this.port = process.env.PORT || 2020;
 
     const server = new Server(this.port);
     server.start();
