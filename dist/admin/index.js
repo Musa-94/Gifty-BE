484d0033ecfde56cebd0d8ad5cad6ed8d76b1b84
MusaW <musa@wizardsdev.com>
2020-08-12 15:46:17 +0300

    added qid at questions

--- dist/admin/index.js
+++ dist/admin/index.js
@@ -81,56 +81,93 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
 /************************************************************************/
-/******/ ({
-
-/***/ "./src/REST/REST.js":
-/*!**************************!*\
-  !*** ./src/REST/REST.js ***!
-  \**************************/
-/*! exports provided: sendQuestion */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"sendQuestion\", function() { return sendQuestion; });\nfunction sendQuestion(question) {\n  var url = \"\".concat(window.location.href, \"addNewQuestion\");\n  fetch(url, {\n    method: 'POST',\n    mode: 'same-origin',\n    headers: {\n      'Content-Type': 'application/json'\n    },\n    body: JSON.stringify(question)\n  }).then(function (response) {\n    return response.ok && console.log('ВОПРОС СОХРАНЁН НА СЕРВЕРЕ');\n  })[\"catch\"](function (err) {\n    return console.error('ERROR', err);\n  });\n}\n\n//# sourceURL=webpack:///./src/REST/REST.js?");
-
-/***/ }),
-
-/***/ "./src/index.js":
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-/*! no exports provided */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_less__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.less */ \"./src/style.less\");\n/* harmony import */ var _style_less__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_style_less__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _inputsTarget__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./inputsTarget */ \"./src/inputsTarget.js\");\n/* harmony import */ var _REST_REST__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./REST/REST */ \"./src/REST/REST.js\");\n\n\n\n_inputsTarget__WEBPACK_IMPORTED_MODULE_1__[\"saveButton\"].addEventListener('click', saveQuestion);\n\nfunction saveQuestion() {\n  var isEmptyInputs = isEmptyValue();\n  var getQuestion = isEmptyInputs ? alert('НЕ ВСЕ ПОЛЯ ЗАПОЛНЕНЫ!') : getQuestionData();\n  getQuestion && Object(_REST_REST__WEBPACK_IMPORTED_MODULE_2__[\"sendQuestion\"])(getQuestion);\n}\n\nfunction getQuestionData() {\n  var questionData = {};\n  questionData.question = _inputsTarget__WEBPACK_IMPORTED_MODULE_1__[\"question\"].value;\n  questionData.answerA = _inputsTarget__WEBPACK_IMPORTED_MODULE_1__[\"answerA\"].value;\n  questionData.answerB = _inputsTarget__WEBPACK_IMPORTED_MODULE_1__[\"answerB\"].value;\n  questionData.answerC = _inputsTarget__WEBPACK_IMPORTED_MODULE_1__[\"answerC\"].value;\n  questionData.answerD = _inputsTarget__WEBPACK_IMPORTED_MODULE_1__[\"answerD\"].value;\n  questionData.answerRight = _inputsTarget__WEBPACK_IMPORTED_MODULE_1__[\"answerRight\"].value;\n  questionData.imageURL = _inputsTarget__WEBPACK_IMPORTED_MODULE_1__[\"imageURL\"].value;\n  clearInputsValue();\n  return questionData;\n}\n\nfunction isEmptyValue() {\n  return _inputsTarget__WEBPACK_IMPORTED_MODULE_1__[\"question\"].value === '' || _inputsTarget__WEBPACK_IMPORTED_MODULE_1__[\"answerA\"].value === '' || _inputsTarget__WEBPACK_IMPORTED_MODULE_1__[\"answerB\"].value === '' || _inputsTarget__WEBPACK_IMPORTED_MODULE_1__[\"answerC\"].value === '' || _inputsTarget__WEBPACK_IMPORTED_MODULE_1__[\"answerD\"].value === '' || _inputsTarget__WEBPACK_IMPORTED_MODULE_1__[\"answerRight\"].value === '' || _inputsTarget__WEBPACK_IMPORTED_MODULE_1__[\"imageURL\"].value === '';\n}\n\nfunction clearInputsValue() {\n  _inputsTarget__WEBPACK_IMPORTED_MODULE_1__[\"question\"].value = '';\n  _inputsTarget__WEBPACK_IMPORTED_MODULE_1__[\"answerA\"].value = '';\n  _inputsTarget__WEBPACK_IMPORTED_MODULE_1__[\"answerB\"].value = '';\n  _inputsTarget__WEBPACK_IMPORTED_MODULE_1__[\"answerC\"].value = '';\n  _inputsTarget__WEBPACK_IMPORTED_MODULE_1__[\"answerD\"].value = '';\n  _inputsTarget__WEBPACK_IMPORTED_MODULE_1__[\"answerRight\"].value = '';\n  _inputsTarget__WEBPACK_IMPORTED_MODULE_1__[\"imageURL\"].value = '';\n}\n\n//# sourceURL=webpack:///./src/index.js?");
-
-/***/ }),
-
-/***/ "./src/inputsTarget.js":
-/*!*****************************!*\
-  !*** ./src/inputsTarget.js ***!
-  \*****************************/
-/*! exports provided: question, answerA, answerB, answerC, answerD, answerRight, imageURL, saveButton */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"question\", function() { return question; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"answerA\", function() { return answerA; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"answerB\", function() { return answerB; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"answerC\", function() { return answerC; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"answerD\", function() { return answerD; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"answerRight\", function() { return answerRight; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"imageURL\", function() { return imageURL; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"saveButton\", function() { return saveButton; });\nvar question = document.getElementById('question');\nvar answerA = document.getElementById('answerA');\nvar answerB = document.getElementById('answerB');\nvar answerC = document.getElementById('answerC');\nvar answerD = document.getElementById('answerD');\nvar answerRight = document.getElementById('answerRight');\nvar imageURL = document.getElementById('imageURL');\nvar saveButton = document.getElementById('saveQuestion');\n\n//# sourceURL=webpack:///./src/inputsTarget.js?");
-
-/***/ }),
-
-/***/ "./src/style.less":
-/*!************************!*\
-  !*** ./src/style.less ***!
-  \************************/
-/*! no static exports found */
+/******/ ([
+/* 0 */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./src/style.less?");
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+
+// EXTERNAL MODULE: ./src/style.less
+var style = __webpack_require__(0);
+
+// CONCATENATED MODULE: ./src/inputsTarget.js
+var question = document.getElementById('question');
+var answerA = document.getElementById('answerA');
+var answerB = document.getElementById('answerB');
+var answerC = document.getElementById('answerC');
+var answerD = document.getElementById('answerD');
+var answerRight = document.getElementById('answerRight');
+var imageURL = document.getElementById('imageURL');
+var saveButton = document.getElementById('saveQuestion');
+// CONCATENATED MODULE: ./src/REST/REST.js
+function sendQuestion(question) {
+  var url = "".concat(window.location.href, "addNewQuestion");
+  fetch(url, {
+    method: 'POST',
+    mode: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(question)
+  }).then(function (response) {
+    return response.ok && console.log('ВОПРОС СОХРАНЁН НА СЕРВЕРЕ');
+  })["catch"](function (err) {
+    return console.error('ERROR', err);
+  });
+}
+// CONCATENATED MODULE: ./src/index.js
+
+
+
+saveButton.addEventListener('click', saveQuestion);
+
+function saveQuestion() {
+  var isEmptyInputs = isEmptyValue();
+  var getQuestion = isEmptyInputs ? alert('НЕ ВСЕ ПОЛЯ ЗАПОЛНЕНЫ!') : getQuestionData();
+  getQuestion && sendQuestion(getQuestion);
+}
+
+function getQuestionData() {
+  var questionData = {};
+  questionData.question = question.value;
+  questionData.answerA = answerA.value;
+  questionData.answerB = answerB.value;
+  questionData.answerC = answerC.value;
+  questionData.answerD = answerD.value;
+  questionData.answerRight = answerRight.value;
+  questionData.imageURL = imageURL.value;
+  questionData.qid = generateQid();
+  clearInputsValue();
+  return questionData;
+}
+
+function generateQid() {
+  return new Date().getTime();
+}
+
+function isEmptyValue() {
+  return question.value === '' || answerA.value === '' || answerB.value === '' || answerC.value === '' || answerD.value === '' || answerRight.value === '' || imageURL.value === '';
+}
+
+function clearInputsValue() {
+  question.value = '';
+  answerA.value = '';
+  answerB.value = '';
+  answerC.value = '';
+  answerD.value = '';
+  answerRight.value = '';
+  imageURL.value = '';
+}
 
 /***/ })
-
-/******/ });
+/******/ ]);
