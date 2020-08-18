fe9b62f1e7015f8d424458afde78e2c450a28fe4
MusaW <musa@wizardsdev.com>
2020-08-19 00:03:42 +0300

    refactoring

--- src/Controller.js
+++ src/Controller.js
@@ -5,7 +5,7 @@ class Controller {
 
     getUnusedQuestion = (question, userUsedQuestionsId) => question.filter(questions => !userUsedQuestionsId.includes(questions.qid));
 
-    randomStartIndex = questionsLength => Math.floor(Math.random() * questionsLength);
+    indexStartRandom = questionsLength => Math.floor(Math.random() * questionsLength);
 
     questionsSortRandomize = question => {
         for (let index = question.length - 1; index > 0; index--) {
@@ -20,11 +20,11 @@ class Controller {
     getTwelveQuestions = questions => {
         const questionsRandomize = this.questionsSortRandomize(questions)
         const questionsQuantity = questionsRandomize.length;
-        const startIndex = this.randomStartIndex(questionsQuantity);
-        const endIndex = startIndex + 12;
-        const twelveQuestions = questionsRandomize.slice(startIndex, endIndex);
+        const indexStart = this.indexStartRandom(questionsQuantity);
+        const indexEnd = indexStart + 12;
+        const twelveQuestions = questionsRandomize.slice(indexStart, indexEnd);
 
-        return twelveQuestions.length < 12 ? questionsRandomize.slice(endIndex, endIndex + (12 - twelveQuestions.length)) : twelveQuestions;
+        return twelveQuestions.length < 12 ? questionsRandomize.slice(indexEnd, indexEnd + (12 - twelveQuestions.length)) : twelveQuestions;
     }
 
     saveUserQuestionsId = (saveQuestions, idUsedQuestions) => {
